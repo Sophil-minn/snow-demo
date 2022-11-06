3dc8db3c66f4162c1fb15f378511f1d3abf13f51
minmin <1056640663@qq.com>
2022-11-06 15:06:56 +0800

    feat:  commander脚手架命令注册的两种方法

--- commander-test/bin/index.js
+++ commander-test/bin/index.js
@@ -18,13 +18,44 @@ program
   .version(pkg.version)
   .option('-d, --debug', '是否开启调试模式', false)
   .option('-e, --envName <envName>', '获取环境变量名称')
-  .parse(process.argv)
+
 
   
   // console.log(program)
-  console.log(program.opts().debug)
-  console.log(program.opts().envName)
-  console.log(program.debug)
-  console.log(program.envName)
-  console.log(11111)
+  // console.log(program.opts().debug)
+  // console.log(program.opts().envName)
+  // console.log(program.debug)
+  // console.log(program.envName)
+  // console.log(11111)
   // program.outputHelp();
+
+  // command 注册命令
+  // 注册命令 command() 返回新对象不是 program 对象
+const clone = program.command('clone <source> [destination]');
+clone
+  .description('clone a repository')
+  .option('-f, --force', '是否强制克隆')
+  .action((source, destination, cmdObj) => {
+    console.log('do clone', source, destination, cmdObj.force);
+  })
+
+  const service = new commander.Command('service');
+  service
+    .command('start [port]')
+    .description('start service at some port')
+    .action(
+      (port, obj) => {
+        console.log('do servie start', port, obj);
+      }
+    );
+    service
+    .command('stop')
+    .description('stop service')
+    .action(() => {
+      console.log('stop service');
+    });
+  
+  program.addCommand(service);
+
+
+program.parse(process.argv)
